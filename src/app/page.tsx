604ba683812f768a531a1f1d62a6e8160dcd42d4
Ghalem Salim <salimghalem40@gmail.com>
2025-07-18 03:06:53 +0100

    testing page for wassim and soundous

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,107 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [houseData] = useState({
+    title: 'Cozy Family Home',
+    description: 'A beautiful and spacious house.',
+    localization: '123 Main St, City',
+    type: 'F4 (4 rooms)',
+    beds: '3',
+    bathrooms: '2',
+    cuisines: '1',
+    price: '$300,000',
+    options: ['wifi', 'parking', 'tv'],
+  });
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'grid',
+        placeItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          width: '50%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 30,
+        }}
+      >
+        <div
+          style={{
+            width: '100%',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(2,1fr)',
+          }}
+        >
+          <Image
+            src="/images/placeholder-house.png"
+            width={300}
+            height={300}
+            alt="Main picture of the house"
+            objectFit="cover"
+          />
+          <div
+            style={{
+              height: '100%',
+              display: 'flex',
+              gap: '20px',
+              flexDirection: 'column',
+            }}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <h2>{houseData.title}</h2>
+            <p>{houseData.description}</p>
+            <p>{houseData.localization}</p>
+            <p>{houseData.type}</p>
+            <p>{houseData.options}</p>
+            <p>Num of beds:{houseData.beds}</p>
+            <p>Num of bathrooms:{houseData.bathrooms}</p>
+            <p>Num of cuisines:{houseData.cuisines}</p>
+            <ul>
+              {houseData.options.map((option) => (
+                <li>{option}</li>
+              ))}
+            </ul>
+            <h3>{houseData.price}</h3>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 50,
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button
+            style={{
+              width: 300,
+              height: 30,
+              backgroundColor: 'green',
+              color: 'white',
+            }}
+          >
+            YAAAAY
+          </button>
+          <button
+            style={{
+              width: 300,
+              height: 30,
+              backgroundColor: 'red',
+              color: 'white',
+            }}
+          >
+            NO
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
